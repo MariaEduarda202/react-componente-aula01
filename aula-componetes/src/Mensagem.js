98d5907167cc438cde639b03ad3f3e1a69128689
Maria EDuarda <mariaeduardaferrreiragomes2021@gmail.com>
2021-10-15 13:56:56 -0500

    aula componentes react

---
+++ aula-componetes/src/Mensagem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export const Mensagem= ()=>{
+    <div>
+        <pre>
+       Quando estiver cansado, <br></br>
+       descanse, não desista.
+
+        </pre>
+    </div>};
